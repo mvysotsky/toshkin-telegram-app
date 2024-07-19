9dae4e0f4896e823d1ee5bb6984a70f74f2a20fe
mvysotsky <m.vysotskyi@gmail.com>
2024-07-20 01:05:48 +0300

    basic layout

--- app/server.js
+++ app/server.js
@@ -1,9 +1,20 @@
 
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 const { message } = require('telegraf/filters')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome RUSLAN!'))
+
+bot.start((ctx) => {
+    ctx.reply('Welcome RUSLAN!', Markup.inlineKeyboard([
+        Markup.button.url('Open Mini App', 'https://url_to_open.com'),
+    ]));
+});
+
+
+
+
+
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
