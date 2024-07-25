9583ffde3206d61e887c7e4e3b39ecdf13f3551b
mvysotsky <m.vysotskyi@gmail.com>
2024-07-25 19:30:35 +0300

    added basic tables

---
+++ docker/database/entrypoint/1-base.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `users`
+(
+    `id`         INT(11)     NOT NULL AUTO_INCREMENT,
+    `username`   VARCHAR(50) NOT NULL COMMENT 'Telegram username',
+    `wallet`     VARCHAR(44) NULL COMMENT 'SOL wallet address',
+    `created_at` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP   NOT NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `username` (`username`)
+);
+
+CREATE TABLE `leadeboard`
+(
+    `id`         INT(11)   NOT NULL AUTO_INCREMENT,
+    `user_id`    INT(11)   NOT NULL,
+    `score`      INT(11)   NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
