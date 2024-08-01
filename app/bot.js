ccf4513412db7757ab510a2d0c20d2a99443412f
mvysotsky <m.vysotskyi@gmail.com>
2024-08-01 17:04:56 +0300

    web app link

--- app/bot.js
+++ app/bot.js
@@ -5,22 +5,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const WEB_APP_URL = "https://toshkin.click";
 
-bot.command("setmenu", ctx =>
-    // sets Web App as the menu button for current chat
-    ctx.setChatMenuButton({
-        text: "Launch",
-        type: "web_app",
-        web_app: { url: WEB_APP_URL },
-    }),
-);
-
 bot.start((ctx) => {
-    ctx.reply('Welcome RUSLAN!');
+    ctx.reply('Welcome RUSLAN!', Markup.inlineKeyboard([
+        Markup.button.webApp('Open Web App', WEB_APP_URL)
+    ]));
 });
-
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 bot.launch();
 
