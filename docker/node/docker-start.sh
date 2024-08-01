b9bbdcccf6299530f5be58e7eaf1ae9d365ce1cd
mvysotsky <m.vysotskyi@gmail.com>
2024-08-01 19:26:17 +0300

    fix node docker

--- docker/node/docker-start.sh
+++ docker/node/docker-start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-DIR="/usr/src/app/node_modules/"
-
-if [ ! -d "$DIR" ]; then
-  echo "Installing node modules in ${DIR}..."
-  npm ci
-fi
+echo "Installing node modules in..."
+npm ci
